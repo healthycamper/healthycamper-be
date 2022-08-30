bda762f8a519970f098b3ac9636ac0188692084d
david palmer <dwppalmer95@gmail.com>
2022-08-29 22:27:45 -0400

    added schemas file

--- src/server/routes/counselor.ts
+++ src/server/routes/counselor.ts
@@ -3,26 +3,25 @@ import {
   FastifyPluginOptions,
   HookHandlerDoneFunction,
 } from "fastify";
+import { schema } from "../schemas";
 
 // write controllers w/ prisma
 // test controllers w/jest
 // write routes w/schema validation
 
-// const routes = (fastify: FastifyInstance, options: FastifyPluginOptions, done: HookHandlerDoneFunction) => {
-//   fastify.addSchema({
-
-//   })
-
-//   fastify.post("/new",
-//     {
-//       body: {
-
-//       }
-//     },
-//     (request, reply) => {
-
-//     })
-// }
+const routes = (
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions,
+  done: HookHandlerDoneFunction
+) => {
+  // fastify.post("/new",
+  //   {
+  //     body: {
+  //     }
+  //   },
+  //   (request, reply) => {
+  //   })
+};
 // structure:
 // schema validation
 // auth(?) - maybe auth is passed in with the options object from the parent
---
+++ src/server/schemas.ts
@@ -0,0 +1,11 @@
+export const schema = [
+  {
+    $id: "counselor",
+    type: "object",
+    properties: {
+      name: { type: "string" },
+      email: { type: "string" },
+      phone: { type: "phone" },
+    },
+  },
+];
